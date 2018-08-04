da9878801c8b9d610081e7467d5f97fe081a1eaa
John Ayad <johnhany97@gmail.com>
2018-08-04 20:02:39 +0100

    IsUnique no extra data structures solution

--- chapter1/1-isUnique.py
+++ chapter1/1-isUnique.py
@@ -22,6 +22,21 @@ def is_unique(s):
     return True
 
 
+def is_unique_2(s):
+    # This method will work without using any extra data structures
+    # Assuming ASCII
+    if len(s) > 128:
+        return False  # Pigeonhole principle
+
+    # Loop through all characters in the word
+    for count_i, char_a in enumerate(s):
+        for count_j, char_b in enumerate(s):
+            if count_i != count_j and char_a == char_b:
+                return False
+
+    return True
+
+
 class Test(unittest.TestCase):
     true_values = [(''), ('abc'), ('123'), ('ab cd'), ('ab12')]
     false_values = [('abab'), ('aa'), ('1238db1'), ('abc abc'),
@@ -32,6 +47,12 @@ class Test(unittest.TestCase):
             self.assertTrue(is_unique(str))
         for str in self.false_values:
             self.assertFalse(is_unique(str))
+
+    def test_unique_2(self):
+        for str in self.true_values:
+            self.assertTrue(is_unique_2(str))
+        for str in self.false_values:
+            self.assertFalse(is_unique_2(str))
 
 
 if __name__ == "__main__":
